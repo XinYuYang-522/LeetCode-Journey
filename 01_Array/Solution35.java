8eddb70405a688b89ad1828921c51dccbe97a30d
yangxinyu <yangxinyu@163.com>
2026-05-21 10:46:17 +0800

    feat: 完成35搜索插入位置，更新README

---
+++ 01_Array/Solution35.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int len = nums.length;
+        int left = 0;
+        int right = len - 1;
+        while(left <= right){
+            int mid = left + ((right - left) >> 1);
+            if (target == nums[mid]){
+                return mid;
+            }
+            else if(target < nums[mid]){
+                right = mid - 1;
+            }
+            else if(target > nums[mid]){
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+}
